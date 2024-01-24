fcdde38a24762db219b00ff11e9cfebe4c40112a
JuanMadrid99 <115056117+JuanMadrid99@users.noreply.github.com>
2024-01-24 09:21:09 -0600

    Update ternario.js

--- ternario.js
+++ ternario.js
@@ -55,3 +55,36 @@ arreglo1[0]=5;
 arreglo1[1]=[[10,11],[12,13]];
 arreglo1[2]={ n1:100, n2:99};
 console.log(arreglo1);
+//////////////////////////////////////////////////Practica//////////////////////////////////////
+// =============================== Ejemplo en funcion con objeto
+var usuario = {
+    "Nombre":"Juan",
+    "Apellido":"Madrid",
+    "Edad":"23",
+    "Altura":"183cm",
+    "Peso":"140",
+    "Nivel Academico": ["Ingenieria","Ingles Intermedio"],
+    "Gustos": { "Comida": "Pizza", 
+                "Color": "Azul"
+                }
+}
+function conocer(dato,elemento){
+/*   if (dato==="Todo" || dato ==="") {
+        return usuario;
+    }
+    if (elemento!== undefined) {
+        return usuario[dato][elemento];
+    }
+    return (usuario[dato]!==undefined)? usuario[dato]: "Dato no Encontrado"; */
+return (dato==="Todo" || dato ==="") ? usuario
+    : (elemento!== undefined)? usuario[dato][elemento] 
+    : (usuario[dato]!==undefined)? usuario[dato]: "Dato no Encontrado" ; 
+}
+
+console.log(conocer("Todo"));
+console.log(conocer("Nombre"));
+console.log(conocer("Nivel Academico",0));
+console.log(conocer("Gustos","Color"));
+console.log(conocer("Grado"));
+console.log(conocer("",1));
+console.log(conocer(""));
