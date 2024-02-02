f9abf358ecf8e10791d9a9469cdd933cc5620f87
JuanMadrid99 <115056117+JuanMadrid99@users.noreply.github.com>
2024-02-01 22:44:11 -0600

    Update and rename arrays.txt to arrays.js

---
+++ Arrays/arrays.js
@@ -0,0 +1,13 @@
+/*En JavaScript, los arreglos son estructuras de datos que te permiten almacenar y organizar elementos de manera secuencial. Pueden contener cualquier tipo de dato, incluyendo números, cadenas, objetos y otras matrices. Los arreglos son objetos de tipo especial en JavaScript y se accede a sus elementos mediante índices. */
+
+// Creación de Arreglos:
+// Puedes crear un arreglo de varias maneras:
+// Forma literal
+const miArray = [1, 2, 3, "cuatro", true];
+// Usando el constructor Array
+const otroArray = new Array(1, 2, 3, "cuatro", true);
+
+// Acceso a Elementos:
+// Los elementos de un arreglo se acceden mediante índices, empezando desde 0:
+console.log(miArray[0]); // Muestra 1
+console.log(miArray[3]); // Muestra "cuatro"
