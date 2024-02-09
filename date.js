6f95e107c14d78889648fa4976db29fc35f4ffdd
JuanMadrid99 <115056117+JuanMadrid99@users.noreply.github.com>
2024-02-08 23:54:58 -0600

    Create date.js

---
+++ date.js
@@ -0,0 +1,17 @@
+const date = new Date(); //Output: 2024-02-08T04:30:18.063Z
+date.getDate(); //Output: 7 
+date.getMonth(); //Output: 1 
+date.getFullYear(); //Output: 2024 
+date.getHours(); //Output: 22 
+date.getMinutes(); //Output: 30 
+date.getSeconds(); //Output: 18 
+date.getMilliseconds(); //Output: 63 
+date.getTime(); //Output: 1707366618063 
+date.setDate(24);  
+date.setMonth(4);  
+date.setFullYear(2025); 
+date.setHours(11);
+date.setMinutes(46); 
+date.setSeconds(50); 
+date.setMilliseconds(176); 
+date.setTime(1648101488177);
